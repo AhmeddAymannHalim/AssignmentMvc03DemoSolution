e0bf99c6e202f57af63f6a4d5eab20a2decb32ff
AhmeddAymannHalim <153849097+AhmeddAymannHalim@users.noreply.github.com>
2024-09-12 19:21:08 +0300

    Part 08 3-Layers Arch. Pattern - Business Logic Layer

---
+++ AssignmentMvc03Demo.BLL/Interfaces/IDepartmentRepository.cs
@@ -0,0 +1,24 @@
+﻿using AssignmentMvc03Demo.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentMvc03Demo.BLL.Interfaces
+{
+    public interface IDepartmentRepository
+    {
+        public IEnumerable<Department> GetAll();
+
+        public Department Get(int id);
+
+        public int Add(Department entity);
+
+        public int Update(Department entity);
+
+        public int Delete(Department entity);
+
+
+    }
+}
