9c91e00a22e7837b5cdfdd217ae026e09f118fc4
AhmeddAymannHalim <153849097+AhmeddAymannHalim@users.noreply.github.com>
2024-09-12 23:00:46 +0300

    Part 11 Department Controller - Create Changed ColoumnType Of Name,Code From "varchar" To "varchar(50)" With Migration andAdded In ModelBase New Properties And DepartmentConfigurations Too From Video ==> Department Module -DAL

---
+++ AssignmentMvc03Demo.DAL/Data/Migrations/20240912194746_ChangingColoumnTypeOfCodeAndName.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AssignmentMvc03Demo.DAL.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangingColoumnTypeOfCodeAndName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Department",
+                type: "varchar(50)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Code",
+                table: "Department",
+                type: "varchar(20)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Department",
+                type: "varchar",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Code",
+                table: "Department",
+                type: "varchar",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(20)");
+        }
+    }
+}
--- AssignmentMvc03Demo.DAL/Models/ModelBase.cs
+++ AssignmentMvc03Demo.DAL/Models/ModelBase.cs
@@ -9,5 +9,15 @@ namespace AssignmentMvc03Demo.DAL.Models
     public class ModelBase
     {
         public int Id { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public int CreatedBy { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public int LastModifiedOn { get; set; }
+
+        public DateTime LastModifiedBy { get; set; }
     }
 }
